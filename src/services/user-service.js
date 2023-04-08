1142d6ebf910c9f86b956b3071f4aba66a694b2a
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-04-08 10:00:00 +0530

    Adedd token generation logic from sign in

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -20,6 +20,29 @@ class UserService{
         }
     }
 
+    async signIn(email, plainPassword){
+        try {
+            // 1 -> fetch the user by email
+            const user = await this.userRepository.getByEmail(email);
+            //2 -> Compare incoming plain pwd with stored encrypted pwd
+            const encryptedpwd = user.password; // var to extrcat encrypted pwd from user obj
+            const passwordMatch = this.checkPassword(plainPassword,encryptedpwd);
+
+            if(!passwordMatch){
+                console.log("Passwords do not match");
+                throw { error:"Incorrect Password"}
+            }
+
+            // 3-> If pwd match create a token and send it to the user
+            const newJWT = this.createToken({ email: user.email , id: user.id });
+            return newJWT;
+            
+        } catch (error) {
+            console.log("Something went wrong in sign up process", error);
+            throw(error)
+        }
+    }
+
      createToken(user){                           // --> This is a normal function 
         try {
             const result = jwt.sign(user,JWT_KEY,{expiresIn:'1h'});
