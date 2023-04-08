98de05ff514625eb7703d282692fcc5d2d2eea55
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-04-08 10:00:00 +0530

    Adedd email and password validation for incoming request

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../../controllers/user-controller');
+const { AuthRequestValidator } = require('../../middlewares/index');
 
-router.post('/signup',UserController.create);
-router.post('/signin',UserController.signIn);
+router.post(
+    '/signup',
+AuthRequestValidator.validateUserAuth,
+UserController.create
+);
+
+router.post(
+    '/signin',
+    AuthRequestValidator.validateUserAuth,
+    UserController.signIn
+);
 
 module.exports = router;
